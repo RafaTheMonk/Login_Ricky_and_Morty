66cabe34c908a50c644b852535dba2b17802d689
RafaTheMonk <rafaelbritovaladares@gmail.com>
2023-08-13 17:24:02 -0300

    Att nas variáveís

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const sanfona = document.querySelector('.sanfona-login');
+const linkLogin = document.querySelector('.link-login');
+const linkRegistro = document.querySelector('.link-register');
+
+linkRegistro.addEventListener('click', ()=>{
+    sanfona.classList.add('active');
+})
